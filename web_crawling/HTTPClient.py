4545a4bfdee9c202a2876038d846dc77839135bf
dapoco <cadizd@proton.me>
2024-10-14 21:11:55 -0500

    made a client

--- web_crawling/HTTPClient.py
+++ web_crawling/HTTPClient.py
@@ -1,1 +1,20 @@
+import urllib.request
+import urllib.error
 
+class HTTPClient():
+    def __init__(self, host):
+        assert isinstance(host, str)
+        self.host = host
+    
+    def fetch(self):
+        try:
+            response = urllib.request.urlopen(self.host)
+            data = response.read()
+            text = data.decode("utf-8")
+            return text
+        
+        except urllib.error.HTTPError as e:
+            print(e)
+
+        except urllib.error.URLError as e:
+            print(e)
---
+++ web_crawling/WebCrawler.py
@@ -0,0 +1,52 @@
+from abc import ABC, abstractmethod
+from HTTPClient import HTTPClient
+import re
+
+class WebCrawler(ABC):
+    # Basic hyperlink pattern: scheme://domain.tld/example/path
+    # HYPERLINK_REGEX_PATTERN = re.compile(r"(?:\w+://)(?:[\w\d]+\.)+(?:[a-z]*)(?:/\w+)*/?")
+    # HYPERLINK_REGEX_PATTERN = re.compile(r"(?:\w+://)(?:[^\s<>().&*^%$#+=]+\.)+(?:[a-z]*)(?:/\w+)*/?")
+    HYPERLINK_REGEX_PATTERN = re.compile(r"(?:\w+://)(?:[a-zA-Z0-9-_]+\.)+(?:[a-z]*)(?:/[a-zA-Z0-9-_]+)*/?")
+
+    def __init__(self, seed, sleep, results_file):
+        assert isinstance(seed, str)
+        assert (isinstance(sleep, int) or isinstance(sleep, float))
+        assert isinstance(results_file, str)
+        assert sleep >= 0
+        self.seed = seed
+        self.sleep = sleep
+        self.results_file = results_file
+        
+    @abstractmethod
+    def crawl(self):
+        pass
+
+    def extract_pattern(self, input, pattern):
+        ''' Return a list of strings matching the pattern, having
+        parsed through input'''
+        matched_groups = pattern.findall(input)
+        return matched_groups
+
+    def extract_links(self, input):
+        return self.extract_pattern(input, WebCrawler.HYPERLINK_REGEX_PATTERN)
+    
+    def extract_emails(self, input):
+        pass
+
+    def write_to_file(self, current_link, links_found):
+        pass
+
+    def get_links(self, current_link):
+        pass
+
+class WebCrawlerThreaded(WebCrawler):
+    def crawl(self):
+        print("crawling....")
+
+# TODO:
+# - We have to write to the file
+    # - get links
+    # - write to file
+    # - create a class that can handle all of these operations!
+# - write our crawl method
+#   - redirect to all links using http client
