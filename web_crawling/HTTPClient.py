0fce93f0f4fe400474f7687747f595c1a3822027
dapoco <cadizd@proton.me>
2024-10-15 16:28:23 -0500

    Added more error checking to prevent processing stoppage in crawler.

--- web_crawling/HTTPClient.py
+++ web_crawling/HTTPClient.py
@@ -1,5 +1,6 @@
 import urllib.request
 import urllib.error
+from http.client import RemoteDisconnected
 
 class HTTPClient():
     def fetch(self, host):
@@ -12,11 +13,17 @@ class HTTPClient():
             data = response.read()
             text = data.decode("utf-8")
             result = text
+
+        except UnicodeDecodeError as e:
+            print("Could not decode data: %s" % e)
         
         except urllib.error.HTTPError as e:
             print(e)
 
         except urllib.error.URLError as e:
             print(e)
+
+        except RemoteDisconnected as e:
+            print("Encountered error for %s" % host)
             
         return result
