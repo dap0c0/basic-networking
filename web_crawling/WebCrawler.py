b7980a4729941387b6838ebb6d67600582189c2a
dapoco <cadizd@proton.me>
2024-10-15 12:24:11 -0500

    Created a FileOrganizer class, a JSON interface for file writing.

--- web_crawling/WebCrawler.py
+++ web_crawling/WebCrawler.py
@@ -1,12 +1,12 @@
 from abc import ABC, abstractmethod
 from HTTPClient import HTTPClient
-import re
+from FileOrganizer import FileOrganizer
 
 class WebCrawler(ABC):
     # Basic hyperlink pattern: scheme://domain.tld/example/path
     # HYPERLINK_REGEX_PATTERN = re.compile(r"(?:\w+://)(?:[\w\d]+\.)+(?:[a-z]*)(?:/\w+)*/?")
     # HYPERLINK_REGEX_PATTERN = re.compile(r"(?:\w+://)(?:[^\s<>().&*^%$#+=]+\.)+(?:[a-z]*)(?:/\w+)*/?")
-    HYPERLINK_REGEX_PATTERN = re.compile(r"(?:\w+://)(?:[a-zA-Z0-9-_]+\.)+(?:[a-z]*)(?:/[a-zA-Z0-9-_]+)*/?")
+    HYPERLINK_REGEX_PATTERN = r"(?:\w+://)(?:[a-zA-Z0-9-_]+\.)+(?:[a-z]*)(?:/[a-zA-Z0-9-_]+)*/?"
 
     def __init__(self, seed, sleep, results_file):
         assert isinstance(seed, str)
@@ -16,11 +16,13 @@ class WebCrawler(ABC):
         self.seed = seed
         self.sleep = sleep
         self.results_file = results_file
-        
+        self.file_interface = FileOrganizer(self.results_file)
+
     @abstractmethod
     def crawl(self):
         pass
 
+    
     def extract_pattern(self, input, pattern):
         ''' Return a list of strings matching the pattern, having
         parsed through input'''
