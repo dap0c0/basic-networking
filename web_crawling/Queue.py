0478d2f4ebd06a777d85543543914ca4a65156c1
dapoco <cadizd@proton.me>
2024-10-15 13:38:15 -0500

    Implemented a very basic Linked List Queue

---
+++ web_crawling/Queue.py
@@ -0,0 +1,74 @@
+from abc import ABC, abstractmethod
+
+class Queue(ABC):
+    @abstractmethod
+    def is_empty(self):
+        pass
+
+    @abstractmethod
+    def enqueue(self, obj):
+        pass
+
+    @abstractmethod
+    def dequeue(self):
+        pass
+
+    @abstractmethod
+    def __repr__(self):
+        pass
+
+class Node():
+    def __init__(self, data, link):
+        self.data = data
+        self.link = link
+
+    def __repr__(self):
+        return f"({self.data}, {self.link})"
+
+class LLQueue(Queue):
+    def __init__(self):
+        self.head = None
+        self.tail = None
+    
+    def is_empty(self):
+        return self.head == None and self.tail == None
+
+    def enqueue(self, obj):
+        assert obj != None
+
+        if self.head == None and self.tail == None:
+            self.head = Node(obj, None)
+            self.tail = self.head
+        
+        else:
+            to_queue = Node(obj, None)
+            self.tail.link = to_queue
+            self.tail = to_queue
+    
+    def dequeue(self):
+        ''' Return head of queue.'''
+        data = None
+
+        if self.head:
+            dequeued = self.head
+            self.head = self.head.link
+
+            if self.head == None:
+                self.tail = None
+
+            if dequeued:
+                data = dequeued.data
+
+        return data
+
+    def __repr__(self):
+        ''' Print the LLQueue's contents'''
+        curr = self.head
+        content = []
+
+        while curr != None:
+            content.append(curr.data)
+            curr = curr.link
+        
+        return str(content)
+
