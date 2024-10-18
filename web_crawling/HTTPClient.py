3a4c6366dcb74cf87dec3a3a341e67b67c5d2777
dapoco <cadizd@proton.me>
2024-10-18 16:51:39 -0500

    Made a class HTTPClientUnblocked (which actually isn't even unblocked, lol). Created a fetch function that allows the generation of http requests.

--- web_crawling/HTTPClient.py
+++ web_crawling/HTTPClient.py
@@ -1,6 +1,7 @@
 import urllib.request
 import urllib.error
 from http.client import RemoteDisconnected
+import socket
 
 class HTTPClient():
     def fetch(self, host):
@@ -27,3 +28,86 @@ class HTTPClient():
             print("Encountered error for %s" % host)
             
         return result
+
+class HTTPClientUnblocked(HTTPClient):
+    HTTP_PORT = 80
+    COM_GET = "GET"
+    COM_POST = "POST"
+    COM_PUT = "PUT"
+    COM_DELETE = "DELETE"
+    DEFAULT_BUFFER_SIZE = 1024
+
+    def __init__(self):
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # self._sock.setblocking(0)
+        self._curr_host = None
+        self._bufsize = HTTPClientUnblocked.DEFAULT_BUFFER_SIZE
+
+    def fetch(self, host, **kargs):
+        assert isinstance(host, str)
+        
+        # Connect to new host if not previously set
+        if host != self._curr_host:
+            self._curr_host = host
+            connecting_addr = (self._curr_host, HTTPClientUnblocked.HTTP_PORT)
+
+            try:
+                self._sock.connect((connecting_addr))
+            
+            except socket.gaierror as e:
+                print("Error connecting to host: %s" % e)
+                return
+            
+        # Send request to host
+        request_buffer = self._generate_request("GET", "/", 1.1, **kargs)
+        print(request_buffer)
+        self._sock.sendall(request_buffer)
+        data = self._recv_data()
+        return data
+
+    def _generate_request(self, command, path, http_vers, **kargs):
+        assert isinstance(command, str)
+        assert command in (self.COM_DELETE, self.COM_GET, self.COM_POST, self.COM_PUT)
+        assert isinstance(path, str)
+        assert isinstance(http_vers, float) or isinstance(http_vers, int)
+        
+        # Generate message in byte format
+        message = ""
+        request_header = "%s %s HTTP/%s\r\n" % (command, path, http_vers)
+        message += request_header
+
+        # Add additional headers as needed
+        if kargs:
+            for karg in kargs:
+                header = karg
+                assert isinstance(header, str)
+                value = kargs[karg]
+                assert isinstance(value, str)
+                header_str = "%s: %s\r\n" % (header, value)
+                message += header_str
+
+        # Assure that double carriage feed is set to mark end of message
+        message += "\r\n"
+        return bytes(message, "utf-8")
+
+    def _recv_data(self):
+        ''' Receive data from the host'''
+        data = b""
+
+        while True:
+            received = self._sock.recv(self._bufsize)
+            print("Len [%d]" % len(received))
+            print(received)
+
+            if len(received) == 0:
+                return data
+
+            data += received
+
+# client = HTTPClientUnblocked()
+# data = client.fetch("youtube.com", Connection="Close")
+# print(data)
+
+
+        
