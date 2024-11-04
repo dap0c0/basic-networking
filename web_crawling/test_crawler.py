1a463d1d8ad3d3710177100a586b3027637db588
dapoco <cadizd@proton.me>
2024-11-04 13:47:44 -0600

    completely discarded httpclient tree of implementations

--- web_crawling/test_crawler.py
+++
@@ -1,7 +0,0 @@
-import WebCrawler
-
-seed = "https://wiki.python.org/moin/HandlingExceptions"
-results_file = "results_a.txt"
-
-spider = WebCrawler.WebCrawlerBasic(seed, results_file)
-spider.crawl(debug=True)
