57305fcc749f616656fd1858df9b2fe29b1fad1b
dapoco <cadizd@proton.me>
2024-11-05 17:48:02 -0600

    Replaced threaded crawl with asynchronous crawl.

--- web_crawling/Crawl.py
+++ web_crawling/Crawl.py
@@ -1,6 +1,6 @@
 import argparse
 # from WebCrawler import profile
-from WebCrawler import WebCrawlerBasic, WebCrawlerThreaded
+from WebCrawler import TwistedWebCrawler, WebCrawlerBasic, WebCrawlerThreaded
 
 def main():
     # def positive(value):
@@ -14,49 +14,41 @@ def main():
     parser.add_argument("-v", "--verbose", action="store_true", dest="debug")
     parser.add_argument("-o", "--output", action="store", dest="output", required=True, type=str)
     parser.add_argument("--sleep", action="store", dest="sleep_time", type=float)
-    parser.add_argument("-t", "--threads", action="store", dest="threads", type=int)
+    parser.add_argument("-a", "--asynchronous", action="store", dest="asynchronous", type=bool, default=True)
+
     args = parser.parse_args()
     seed = args.seed
     debug = args.debug
     output = args.output
     sleep_time = args.sleep_time
-    threads = args.threads
+    asynchronous = args.asynchronous
     crawler = None
 
     # Check if crawler should sleep
     if not sleep_time:
-        if not threads:
+        if not asynchronous:
             crawler = WebCrawlerBasic(seed, output)
 
         else:
-            crawler = WebCrawlerThreaded(seed, output, threads)
-            print("threaded crawler!!!!")
-    
+            crawler = TwistedWebCrawler(seed, output)
+   
     else:
         if sleep_time < 0:
             try: raise argparse.ArgumentTypeError(f"Sleep time must be >= 0") # lol, idk if this is even a practice!
             except argparse.ArgumentTypeError as e:                  # Its just nice that the callstack isn't displayed.
                 print("Error: %s" % e)
 
-        if not threads:
+        if not asynchronous:
             crawler = WebCrawlerBasic(seed, output, sleep_time)
         
         else:
-            crawler = WebCrawlerThreaded(seed, output, sleep_time, threads)
+            crawler = WebCrawlerThreaded(seed, output, sleep_time, asynchronous)
 
     # Unleash the crawler. mwhahaahaahahaha
     if not debug:
-        if not threads:
             crawler.crawl(debug=False)
-        
-        else:
-            crawler.crawl(debug=False, num_threads=threads)
-        
-    else:
-        if not threads:
-            crawler.crawl(debug=True)
 
-        else:
-            crawler.crawl(debug=True, num_threads=threads)
+    else:
+            crawler.crawl(debug=True)
 
 main()
