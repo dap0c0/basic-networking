270c5d30b06b878b597244b8123581e7b3d81906
dapoco <cadizd@proton.me>
2024-11-08 21:00:44 -0600

    Created interface for future chat servers.

---
+++ ChatServer/ChatServer.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class ChatServer(ABC):
+    DEFAULT_BACKLOG = 10000
+    MAX_BACKLOG = 1000000
+
+    def __init__(self, port: int, backlog: int=DEFAULT_BACKLOG):
+        assert isinstance(port, int)
+        assert isinstance(backlog, int)
+        assert port >= 0
+        assert 0 < backlog and backlog <= ChatServer.MAX_BACKLOG, "Backlog is invalid!"
+
+    @abstractmethod
+    def handle_connection(self):
+        pass
+
+    @abstractmethod
+    def data_received(self, data: bytes):
+        pass
+
+    @abstractmethod
+    def connection_closed(self):
+        pass
+        
+
